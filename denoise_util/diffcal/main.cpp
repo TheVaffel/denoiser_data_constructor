9a5a2863ff6e145ab9954f542e822fc424e7cdb3
Håkon Flatval <haakofl@stud.ntnu.no>
2020-06-05 12:39:07 +0200

    Make usage message marginally more informative

--- denoise_util/diffcal/main.cpp
+++ denoise_util/diffcal/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, const char **argv) {
   
   if (argc != 3 || (!is_format(argv[1]) || !is_format(argv[2]))) {
-    std::cerr << "Must call diffcal with two arguments: ./diffcal <format1> <format2>, where each format takes an integer, e.g. image%00d.png" << std::endl;
+    std::cerr << "Must call diffcal with two arguments: ./diffcal <test_image_format> <reference_image_format>, where each format takes an integer, e.g. image%00d.png" << std::endl;
     exit(-1);
   }
 
